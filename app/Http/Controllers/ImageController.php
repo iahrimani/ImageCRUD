d54f7ff81e2e2bc5e61bc94e5c924dba95a38f9e
Дмитрий <ahriman@inbox.ru>
2021-11-16 18:18:09 +0300

    upload project

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Image;
+use Illuminate\Support\Facades\Storage;
+
+class ImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $images = Image::all();
+
+        return view('image', compact('images'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $path = $request->file('path')->store('images');
+        $image = Image::create([
+            'name' => $request->name,
+            'path' => $path,
+            'url' => route('index').  '/storage/' . $path,
+        ]);
+
+        return redirect(route('index'));
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $image = Image::findOrfile($id)->delete();
+
+        return redirect(route('index'));
+    }
+}
---
+++ resources/views/image.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Картинки</title>
+</head>
+<body>
+<div class="container">
+    <br>
+    <br>
+    <h1>Загрузка файлов на сервер</h1>
+    <br>
+    <br>
+    @foreach($images as $image)
+        <img src="{{ $image->url }}" alt="Картнка">
+    @endforeach
+
+    <form class="mt-3" method="post" enctype="multipart/form-data" action="{{ route('store') }}">
+        @csrf
+        <div class="input-group mb-3">
+            <span class="input-group-text">Имя картинки</span>
+            <input type="text" name="name" class="form-control">
+        </div>
+        <div class="input-group">
+            <input type="file" name="path" class="form-control mb-3">
+        </div>
+        <button class="btn btn-outline-secondary" type="submit">Загрузить</button>
+    </form>
+</div>
+</body>
+</html>
